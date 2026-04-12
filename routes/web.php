c1824ad677b54e5f9edafbcae4f301894735352c
PROD\CGriffit <cody.griffith@quest.com>
2026-04-12 17:48:44 +0200

    Add in fallback to no frontend

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::fallback(function () {
-    return File::get(public_path('index.html'));
+    $path = public_path('index.html');
+    if (!File::exists($path)) {
+        return response()->json(['message' => 'Frontend not built, but brain active'], 200);
+    }
+    return File::get($path);
 });
